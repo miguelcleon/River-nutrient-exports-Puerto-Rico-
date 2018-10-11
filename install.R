462b70d826058d09172b5148ae9cd75398b5efa3
Miguel Leon <leonmi@sas.upenn.edu>
2018-10-11 13:04:50 -0400

    update install R
    update install R

--- install.R
+++ install.R
@@ -1,16 +1,14 @@
-# install.packages('devtools')
-# rprofile_path = file.path(Sys.getenv("HOME"), ".Rprofile")
-# write('\noptions(repos=c(getOption(\'repos\'),
-#          CRAN=\'https://cloud.r-project.org\',
-#          USGS=\'https://owi.usgs.gov/R\'))\n',
-#        rprofile_path, 
-#        append =  TRUE)
-# library(devtools)
-# install_version("loadflex", version = "1.0.1", repos=c("https://owi.usgs.gov/R",getOption("repos")))
-# install.packages('loadflex')
-# update.packages('loadflex')
-# available.packages( repos=c("https://owi.usgs.gov/R",getOption("repos")))
+rprofile_path = file.path(Sys.getenv("HOME"), ".Rprofile")
+write('\noptions(repos=c(getOption(\'repos\'),
+          CRAN=\'https://cloud.r-project.org\',
+          USGS=\'https://owi.usgs.gov/R\'))\n',
+      rprofile_path, 
+      append =  TRUE)
+install.packages('smwrBase')
+install.packages('rloadest')
+install.packages('car')
+install.packages('unitted')
+install.packages('dplyr')
 packageurl <- "https://github.com/USGS-R/loadflex/archive/v1.0.1.tar.gz"
 install.packages(packageurl, repos=NULL, type="source")
 install.packages('dplyr')
-library('dplyr')
